03ace9de445db0660fa3c0d8fa2462a3bf07f825
myronmontoya <myronmontoya@gmail.com>
2025-05-21 10:42:55 -0600

    Cree el navbar, y hice el primer index de prodcutos, que solo tiene un Hola mundo

---
+++ controllers/ProductosControlador.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+//use Model\Clientes;
+use Model\ActiveRecord;
+
+class ProductosControlador extends ActiveRecord
+{
+    public static function renderizarPagina(Router $router)
+    {
+        $router->render('productos/index', []);
+    }
+    
+}
--- public/index.php
+++ public/index.php
@@ -1,13 +1,25 @@
 <?php 
 require_once __DIR__ . '/../includes/app.php';
 
+//--aqui se agreega cada vez que hacemos una pagina
 
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\ProductosControlador;
+
+
+//-----a
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
+//Aqui agrego las rutas
+
 $router->get('/', [AppController::class,'index']);
+$router->get('/productos_de_maria', [ProductosControlador::class,'renderizarPagina']);
+
+
+//-------------------------------------------------------
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
